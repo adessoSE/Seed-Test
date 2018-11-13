147a41764f3a1ca5de7ebe1a7c35d436c03dc1b1
Daniel Sorna <daniel.sorna@stud.fh-rosenheim.de>
2018-11-13 12:49:44 +0100

    test Api call on GitHub

---
+++ backend/github-api.js
@@ -0,0 +1,16 @@
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+var data;
+var request = new XMLHttpRequest();
+request.open('GET', 'https://api.github.com/repos/dsorna/cucumber-test/issues');
+request.send();
+
+// testing
+request.onreadystatechange=function(){
+  if (this.readyState===4 && this.status===200){
+    data = JSON.parse(request.responseText);
+    console.log(data[0]["title"] + " " + data[0]["id"])
+    console.log(data[1]["title"] + " " + data[1]["id"])
+  }
+}
+
