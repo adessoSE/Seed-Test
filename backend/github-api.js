a0686306314aa309d331cdd7f7a27f254ec9fff6
Unknown <c-niebergall@web.de>
2018-11-13 12:53:33 +0100

    my first commit

--- backend/github-api.js
+++ backend/github-api.js
@@ -5,7 +5,7 @@ var request = new XMLHttpRequest();
 request.open('GET', 'https://api.github.com/repos/dsorna/cucumber-test/issues');
 request.send();
 
-// testing
+// testing, test2
 request.onreadystatechange=function(){
   if (this.readyState===4 && this.status===200){
     data = JSON.parse(request.responseText);
