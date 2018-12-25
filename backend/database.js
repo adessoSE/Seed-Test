f71dbf881e3946d8e738bde4743d01a40f0583e0
Daniel Sorna <daniel.sorna@adesso.de>
2018-12-25 17:59:58 +0100

    changed scripts - congif eslint

--- backend/database.js
+++ backend/database.js
@@ -1,189 +1,404 @@
-var loki = require('lokijs');
-var db = new loki('db.json');
-var stories = db.addCollection('Stories');
+const loki = require('lokijs');
+const db = new loki('db.json');
+const stories = db.addCollection('Stories');
+const stepDefinitions = db.addCollection('StepDefinitions');
 
 // Test Data
 stories.insert([
-    {story_id: 386695799 , scenarios: [
-        {scenario_id: 1,
-        name: 'successful Login' ,
+  {
+    story_id: 386695799, scenarios: [
+      {
+        scenario_id: 1,
+        name: 'successful Login',
         stepDefinitions: [
-            {given: [
-                {id: 1 , name: 'Guest' , type: 'Role' , pre: 'As a' , mid: '' , post: '' , values: []}
-            ] ,
+          {
+            given: [
+              {id: 1, name: 'Guest', type: 'Role', pre: 'As a', mid: '', post: '', values: []}
+            ],
             when: [
-                {id: 1 , name: 'www.addeso.de' , type: 'Website' , pre: 'I want to visit this site:' , mid: '' , post: '' , values: ['']} ,
-                {id: 2 , name: 'Login' , type: 'Button' , pre: 'I want to click the Button:' , mid: '' , post: '' , values: ['']} ,
-                {id: 3 , name: 'Pets' , type: 'Checkbox' , pre: 'I want to select multiple Values for:' , mid: '' , post: '' , values: ['Cat', 'Dog', 'Spider']}
-        ],
+              {
+                id: 1,
+                name: 'www.addeso.de',
+                type: 'Website',
+                pre: 'I want to visit this site:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 2,
+                name: 'Login',
+                type: 'Button',
+                pre: 'I want to click the Button:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 3,
+                name: 'Pets',
+                type: 'Checkbox',
+                pre: 'I want to select multiple Values for:',
+                mid: '',
+                post: '',
+                values: ['Cat', 'Dog', 'Spider']
+              }
+            ],
             then: [
-                {id: 1 , name: 'www.adesso.de/myProfile' , type: 'Website' , pre: 'So I will be navigated to:' , mid: '' , post: '' , values: ['']} ,
-                {id: 2 , name: 'Validation' , type: 'Text' , pre: 'So i can see the Text:' , mid: '' , post: '' , values: ['Successfully logged in']}
+              {
+                id: 1,
+                name: 'www.adesso.de/myProfile',
+                type: 'Website',
+                pre: 'So I will be navigated to:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 2,
+                name: 'Validation',
+                type: 'Text',
+                pre: 'So i can see the Text:',
+                mid: '',
+                post: '',
+                values: ['Successfully logged in']
+              }
             ]
-        }] } ,
-        {scenario_id: 2,
-        name: 'failed Login' ,
+          }]
+      },
+      {
+        scenario_id: 2,
+        name: 'failed Login',
         stepDefinitions: [
-            {given: [
-                {id: 1 , name: 'User' , type: 'Role' , pre: 'As a' , mid: '' , post: '' , values: ['Mustermann' , 'Geheim666']}
-            ] ,
+          {
+            given: [
+              {id: 1, name: 'User', type: 'Role', pre: 'As a', mid: '', post: '', values: ['Mustermann', 'Geheim666']}
+            ],
             when: [
-                {id: 1 , name: 'www.gamestar.de' , type: 'Website' , pre: 'I want to visite this site:' , mid: '' , post: '' , values: ['']} ,
-                {id: 2 , name: 'Login' , type: 'Button' , pre: 'I want to click the Button:' , mid: '' , post: '' , values: ['']} ,
-                {id: 3 , name: 'Games' , type: 'individual_selection' , pre: 'I want to select:' , mid: '' , post: '' , values: ['Rpg']}
-        ],
+              {
+                id: 1,
+                name: 'www.gamestar.de',
+                type: 'Website',
+                pre: 'I want to visite this site:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 2,
+                name: 'Login',
+                type: 'Button',
+                pre: 'I want to click the Button:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 3,
+                name: 'Games',
+                type: 'individual_selection',
+                pre: 'I want to select:',
+                mid: '',
+                post: '',
+                values: ['Rpg']
+              }
+            ],
             then: [
-                {id: 1 , name: 'www.gamestar.de/login' , type: 'Website' , pre: 'So I will be navigated to:' , mid: '' , post: '' , values: ['']} ,
-                {id: 2 , name: 'Validation' , type: 'Text' , pre: 'So i can see the Text:' , mid: '' , post: '' , values: ['Password or User incorrect']}
+              {
+                id: 1,
+                name: 'www.gamestar.de/login',
+                type: 'Website',
+                pre: 'So I will be navigated to:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 2,
+                name: 'Validation',
+                type: 'Text',
+                pre: 'So i can see the Text:',
+                mid: '',
+                post: '',
+                values: ['Password or User incorrect']
+              }
             ]
 
-        }]}]},
-        {story_id: 386697647 , scenarios: [
-            {scenario_id: 1,
-            name: 'sign up' ,
-            stepDefinitions: [
-                {given: [
-                    {id: 1 , name: 'Guest' , type: 'Role' , pre: 'As a' , mid: '' , post: '' , values: []}
-                ] ,
-                when: [
-                    {id: 1 , name: 'www.abc.de' , type: 'Website' , pre: 'I want to visit this site:' , mid: '' , post: '' , values: ['']} ,
-                    {id: 2 , name: 'Username' , type: 'Website' , pre: 'I want to insert:' , mid: '' , post: '' , values: ['Mustermann']} ,
-                    {id: 3 , name: 'Password' , type: 'Website' , pre: 'I want to insert:' , mid: '' , post: '' , values: ['Geheim123']} ,
-                    {id: 4 , name: 'SignUp' , type: 'Button' , pre: 'I want to click the Button:' , mid: '' , post: '' , values: ['']}
+          }]
+      }]
+  },
+  {
+    story_id: 386697647, scenarios: [
+      {
+        scenario_id: 1,
+        name: 'sign up',
+        stepDefinitions: [
+          {
+            given: [
+              {id: 1, name: 'Guest', type: 'Role', pre: 'As a', mid: '', post: '', values: []}
             ],
-                then: [
-                    {id: 1 , name: 'www.abc.de/myProfile' , type: 'Website' , pre: 'So I will be navigated to:' , mid: '' , post: '' , values: ['']} ,
-                    {id: 2 , name: 'Validation' , type: 'Text' , pre: 'So i can see the Text:' , mid: '' , post: '' , values: ['Successfully signed up']}
-                ]
+            when: [
+              {
+                id: 1,
+                name: 'www.abc.de',
+                type: 'Website',
+                pre: 'I want to visit this site:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 2,
+                name: 'Username',
+                type: 'Website',
+                pre: 'I want to insert:',
+                mid: '',
+                post: '',
+                values: ['Mustermann']
+              },
+              {
+                id: 3,
+                name: 'Password',
+                type: 'Website',
+                pre: 'I want to insert:',
+                mid: '',
+                post: '',
+                values: ['Geheim123']
+              },
+              {
+                id: 4,
+                name: 'SignUp',
+                type: 'Button',
+                pre: 'I want to click the Button:',
+                mid: '',
+                post: '',
+                values: ['']
+              }
+            ],
+            then: [
+              {
+                id: 1,
+                name: 'www.abc.de/myProfile',
+                type: 'Website',
+                pre: 'So I will be navigated to:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 2,
+                name: 'Validation',
+                type: 'Text',
+                pre: 'So i can see the Text:',
+                mid: '',
+                post: '',
+                values: ['Successfully signed up']
+              }
+            ]
 
-            }] } ,
-            {scenario_id: 2,
-            name: 'user already exist' ,
-            stepDefinitions: [
-                {given: [
-                    {id: 1 , name: 'Guest' , type: 'Role' , pre: 'As a' , mid: '' , post: '' , values: ['']}
-                ] ,
-                when: [
-                    {id: 1 , name: 'www.abc.de' , type: 'Website' , pre: 'I want to visit this site:' , mid: '' , post: '' , values: ['']} ,
-                    {id: 2 , name: 'Username' , type: 'Website' , pre: 'I want to insert:' , mid: '' , post: '' , values: ['Mustermann']} ,
-                    {id: 3 , name: 'Password' , type: 'Website' , pre: 'I want to insert:' , mid: '' , post: '' , values: ['Geheim123']} ,
-                    {id: 4 , name: 'SignUp' , type: 'Button' , pre: 'I want to click the Button:' , mid: '' , post: '' , values: ['']} ,
+          }]
+      },
+      {
+        scenario_id: 2,
+        name: 'user already exist',
+        stepDefinitions: [
+          {
+            given: [
+              {id: 1, name: 'Guest', type: 'Role', pre: 'As a', mid: '', post: '', values: ['']}
+            ],
+            when: [
+              {
+                id: 1,
+                name: 'www.abc.de',
+                type: 'Website',
+                pre: 'I want to visit this site:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 2,
+                name: 'Username',
+                type: 'Website',
+                pre: 'I want to insert:',
+                mid: '',
+                post: '',
+                values: ['Mustermann']
+              },
+              {
+                id: 3,
+                name: 'Password',
+                type: 'Website',
+                pre: 'I want to insert:',
+                mid: '',
+                post: '',
+                values: ['Geheim123']
+              },
+              {
+                id: 4,
+                name: 'SignUp',
+                type: 'Button',
+                pre: 'I want to click the Button:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
 
             ],
-                then: [
-                    {id: 1 , name: 'www.abc.de' , type: 'Website' , pre: 'So I will be navigated to:' , mid: '' , post: '' , values: ['']} ,
-                    {id: 2 , name: 'Validation' , type: 'Text' , pre: 'So i can see the Text:' , mid: '' , post: '' , values: ['User already exists']}
-                ]
+            then: [
+              {
+                id: 1,
+                name: 'www.abc.de',
+                type: 'Website',
+                pre: 'So I will be navigated to:',
+                mid: '',
+                post: '',
+                values: ['']
+              },
+              {
+                id: 2,
+                name: 'Validation',
+                type: 'Text',
+                pre: 'So i can see the Text:',
+                mid: '',
+                post: '',
+                values: ['User already exists']
+              }
+            ]
 
-            }] } ]},
+          }]
+      }]
+  },
 
 ])
 
 // Step Definitions
-var stepDefinitions = db.addCollection('StepDefinitions');
-
 stepDefinitions.insert([
-    {stepType: 'given', name: '' , type: 'Role' , pre: 'As a' , mid: '', post: '', values:[] , selection: ['Guest' , 'User']} ,
-    {stepType: 'when' , name: '' , type: 'Website' , pre: 'I want to visit this site:' , mid: '', post: '', values:[] } ,
-    {stepType: 'when' , name: '' , type: 'Button' , pre: 'I want to click the Button:' , mid: '', post: '', values:[] } ,
-    {stepType: 'when' , name: '' , type: 'Field' , pre: 'I want to insert:' , mid: '', post: '', values:[] } ,
-    {stepType: 'when' , name: '' , type: 'Radio (Individual Selection)' , pre: 'I want to select:' , mid: '', post: '', values:[] } ,
-    {stepType: 'when' , name: '' , type: 'Checkbox' , pre: 'I want to select multiple Values for:' , mid: '', post: '', values:[] } ,
-    {stepType: 'then' , name: '' , type: 'Website' , pre: 'So I will be navigated to:' , mid: '', post: '', values:[] } ,
-    {stepType: 'then' , name: '' , type: 'Text' , pre: 'So i can see the Text:' , mid: '', post: '', values:[] }
+  {stepType: 'given', name: '', type: 'Role', pre: 'As a', mid: '', post: '', values: [], selection: ['Guest', 'User']},
+  {stepType: 'when', name: '', type: 'Website', pre: 'I want to visit this site:', mid: '', post: '', values: []},
+  {stepType: 'when', name: '', type: 'Button', pre: 'I want to click the Button:', mid: '', post: '', values: []},
+  {stepType: 'when', name: '', type: 'Field', pre: 'I want to insert:', mid: '', post: '', values: []},
+  {
+    stepType: 'when',
+    name: '',
+    type: 'Radio (Individual Selection)',
+    pre: 'I want to select:',
+    mid: '',
+    post: '',
+    values: []
+  },
+  {
+    stepType: 'when',
+    name: '',
+    type: 'Checkbox',
+    pre: 'I want to select multiple Values for:',
+    mid: '',
+    post: '',
+    values: []
+  },
+  {stepType: 'then', name: '', type: 'Website', pre: 'So I will be navigated to:', mid: '', post: '', values: []},
+  {stepType: 'then', name: '', type: 'Text', pre: 'So i can see the Text:', mid: '', post: '', values: []}
 
 ])
 
 // Mockups for empty Story and Scenarios
-var emptyStory = {story_id: '' , scenarios: [
-    {scenario_id: 1,
-    name: '' ,
-    stepDefinitions: [
-        {given: [] ,
-        when: [],
-        then: []
-    }]} ]}
-
-var emptyScenario = {
-name: 'New Scenario' ,
-stepDefinitions: [
+const emptyStory = {
+  story_id: '', scenarios: [
     {
-      given: [] ,
+      scenario_id: 1,
+      name: '',
+      stepDefinitions: [
+        {
+          given: [],
+          when: [],
+          then: []
+        }]
+    }]
+}
+
+const emptyScenario = {
+  name: 'New Scenario',
+  stepDefinitions: [
+    {
+      given: [],
       when: [],
       then: []
     }
-]}
-
-// GET all stories TODO: unused right now
-function showStories() {
-    stories.find()
+  ]
 }
+
+// // GET all stories TODO: unused right now
+// function showStories() {
+//     stories.find()
+// }
 // GET all StepDefinitions
 function showStepdefinitions() {
-    return stepDefinitions.find()
+  return stepDefinitions.find()
 }
 
 // Create SCENARIO
-function createScenario (git_id) {
-    var story = stories.findOne({story_id: git_id});
-    var lastScenarioIndex = story.scenarios.length
-    var tmpScenario = emptyScenario;
+function createScenario(git_id) {
+  let story = stories.findOne({story_id: git_id});
+  let lastScenarioIndex = story.scenarios.length
+  let tmpScenario = emptyScenario;
 
-    if (story != null) {
-            tmpScenario.scenario_id = story.scenarios[lastScenarioIndex-1].scenario_id + 1
-            console.log("scenario id", tmpScenario.scenario_id);
-            tmpScenario.name = 'New Scenario'
-      // todo here is something wrong -> if u add multiple stories the latest id will be applied to all new stories
-            stories.chain().find({story_id: git_id}).update(function(obj){
-              obj.scenarios.push(tmpScenario)
-        })
-    } else {
-        var tmpStory = emptyStory
-        tmpStory.story_id = git_id
-        stories.insert(tmpStory)
-    }
-    return true;
+  if (story != null) {
+    tmpScenario.scenario_id = story.scenarios[lastScenarioIndex - 1].scenario_id + 1
+    console.log("scenario id", tmpScenario.scenario_id);
+    tmpScenario.name = 'New Scenario'
+    // todo here is something wrong -> if u add multiple stories the latest id will be applied to all new stories
+    stories.chain().find({story_id: git_id}).update(function (obj) {
+      obj.scenarios.push(tmpScenario)
+    })
+  } else {
+    let tmpStory = emptyStory
+    tmpStory.story_id = git_id
+    stories.insert(tmpStory)
+  }
+  return true;
 }
 
 // POST SCENARIO //TODO: create new scenario, if there is none with the ID?
-function updateScenario (git_id, scenario) {
+function updateScenario(git_id, scenario) {
   let success = false;
-    var story = stories.findOne({story_id: git_id});
-    if (story != null) {
-        stories
-        .chain()
-        .find({ "story_id" : git_id})
-        .where(function(obj) {
-            for (let i in obj.scenarios){
-                if(obj.scenarios[i].scenario_id === scenario.scenario_id){
-                   obj.scenarios.splice(i,1,scenario);
-                   success = true;
-                   break;
-                }
-            }
-        })
-    }
+  let story = stories.findOne({story_id: git_id});
+  if (story != null) {
+    stories
+      .chain()
+      .find({"story_id": git_id})
+      .where(function (obj) {
+        for (let i in obj.scenarios) {
+          if (obj.scenarios[i].scenario_id === scenario.scenario_id) {
+            obj.scenarios.splice(i, 1, scenario);
+            success = true;
+            break;
+          }
+        }
+      })
+  }
   return success;
 }
 
 // DELETE Scenario
-function deleteScenario (git_id, s_id ) {
+function deleteScenario(git_id, s_id) {
   let sucess = false;
-    stories
-        .chain()
-        .find({ "story_id" : git_id})
-        .where(function(obj) {
-            for (var i = 0; i < obj.scenarios.length; i++){
-                if(obj.scenarios[i].scenario_id == s_id){
-                    obj.scenarios.splice(i, 1)
-                    console.log("scenario deleted!");
-                    sucess = true;
-                    break
-                }
-            }
-        });
-    return sucess;
+  stories
+    .chain()
+    .find({"story_id": git_id})
+    .where(function (obj) {
+      for (let i = 0; i < obj.scenarios.length; i++) {
+        if (obj.scenarios[i].scenario_id == s_id) {
+          obj.scenarios.splice(i, 1)
+          console.log("scenario deleted!");
+          sucess = true;
+          break
+        }
+      }
+    });
+  return sucess;
 }
 
-module.exports = {stories: stories, showStepdefinitions: showStepdefinitions,
-  createScenario: createScenario, deleteScenario: deleteScenario, updateScenario: updateScenario};
+module.exports = {
+  stories: stories, showStepdefinitions: showStepdefinitions,
+  createScenario: createScenario, deleteScenario: deleteScenario, updateScenario: updateScenario
+};
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,24 @@
-let express = require('express');
-let cors = require('cors');
-let bodyParser = require("body-parser");
-let app = express();
-let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-let db = require('./database');
-let stories_db = require('./database').stories;
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+const app = express();
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+const db = require('./database');
+const stories_db = require('./database').stories;
+const process = require('process');
+
 
 // Initialize the app.
-let server = app.listen(process.env.PORT || 8080, function () {
+const server = app.listen(process.env.PORT || 8080, function () {
   let port = server.address().port;
   console.log("App now running on port", port);
 });
 
 // TODO: priority 2 for every api
-function handleError(res, reason, statusMessage, code) {
-  console.log("ERROR: " + reason);
-  res.status(code || 500).json({"error": statusMessage});
-}
+// function handleError(res, reason, statusMessage, code) {
+//   console.log("ERROR: " + reason);
+//   res.status(code || 500).json({"error": statusMessage});
+// }
 
 /**
  * API Description
@@ -27,7 +29,7 @@ app
   .use(bodyParser.urlencoded({limit: '100kb', extended: true}))
   .use(function (req, res, next) {
     console.log('Time:', Date.now());
-    console.log('%r %s %n',req, res, next);
+    console.log('%r %s %n', req, res, next);
     next();
   })
   .get("/api", function (req, res) {
@@ -48,7 +50,7 @@ app
   /**
    * Scenarios API
    */
-  .get("/api/stepDefinitions", function (req, res, next) {
+  .get("/api/stepDefinitions", function (req, res) {
     res.status(200).json(db.showStepdefinitions());
   })
   .get("/api/stories", function (req, res) {
@@ -56,24 +58,24 @@ app
     let request = new XMLHttpRequest();
     request.open('GET', 'https://api.github.com/repos/fr4gstar/Cucumber/issues?labels=story');
     request.send();
-    request.onreadystatechange=function(){
-      if (this.readyState===4 && this.status===200){
+    request.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
         let data = JSON.parse(request.responseText);
         // init result
         let stories = [];
-        for(let issue of data) {
+        for (let issue of data) {
           // only relevant issues with label: "story"
-            let story = {story_id: issue["id"], title: issue["title"], body: issue["body"], state: issue['state']};
-            if (issue["assignee"] !== null) { // skip in case of "unassigned"
-              story["assignee"] = issue["assignee"]["login"];
-              story["assignee_avatar_url"] = issue["assignee"]["avatar_url"];
-            }
-            if (stories_db.findOne({story_id: story.story_id}) != null) { // skip if there is no data for the issue yet
-              story["scenarios"] = stories_db.findOne({story_id: story.story_id}).scenarios;
-            }
-            stories_db.insert(story); // update database
-            //TODO: delete stories priority 2
-            stories.push(story);
+          let story = {story_id: issue["id"], title: issue["title"], body: issue["body"], state: issue['state']};
+          if (issue["assignee"] !== null) { // skip in case of "unassigned"
+            story["assignee"] = issue["assignee"]["login"];
+            story["assignee_avatar_url"] = issue["assignee"]["avatar_url"];
+          }
+          if (stories_db.findOne({story_id: story.story_id}) != null) { // skip if there is no data for the issue yet
+            story["scenarios"] = stories_db.findOne({story_id: story.story_id}).scenarios;
+          }
+          stories_db.insert(story); // update database
+          //TODO: delete stories priority 2
+          stories.push(story);
         }
         res.status(200).json(stories);
         console.log("Returning stories.")
@@ -81,11 +83,11 @@ app
     };
   })
   // create scenario
-  .get("/api/scenario/add/:issueID", function(req, res) {
-    if (db.createScenario(parseInt(req.params.issueID))){
+  .get("/api/scenario/add/:issueID", function (req, res) {
+    if (db.createScenario(parseInt(req.params.issueID))) {
       res.status(200);
       console.log("Scenario created.");
-    }else {
+    } else {
       console.log("Could not create scenario.");
       res.status(500);
     }
@@ -95,10 +97,10 @@ app
     // TODO use model to check for scenario (priority 2)
     let scenario = req.body;
     console.log("Trying to update scenario in issue: " + req.params.issueID + " with ID: " + scenario.scenario_id);
-    if (db.updateScenario(parseInt(req.params.issueID), scenario)){
+    if (db.updateScenario(parseInt(req.params.issueID), scenario)) {
       res.status(200);
       console.log("Scenario updated.");
-    }else {
+    } else {
       console.log("Could not update the scenario.");
       res.status(500);
     }
@@ -109,7 +111,7 @@ app
     if (db.deleteScenario(parseInt(req.params.issueID), req.params.scenarioID)) {
       res.status(200).json({});
       console.log("Scenario deleted.");
-    }else {
+    } else {
       console.log("Could not delete Scenario.");
       res.status(500).json({});
     }
