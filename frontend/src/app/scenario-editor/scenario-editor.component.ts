18a5d61d1af042716a55fa742008c70bcfbd3db0
Jonas Schreier <jonas.schreier164@gmail.com>
2019-02-07 16:16:12 +0100

    New Steps are now created with increasing IDs. ID is always one higher then the ID of the last element in the given/when/then array. This does not 100% guarantee unique ids in their respective array, but should be good enough.

--- frontend/src/app/scenario-editor/scenario-editor.component.ts
+++ frontend/src/app/scenario-editor/scenario-editor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {ApiService} from '../Services/api.service';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../Services/api.service';
+import { getComponentViewDefinitionFactory } from '@angular/core/src/view';
 @Component({
   selector: 'app-scenario-editor',
   templateUrl: './scenario-editor.component.html',
@@ -15,7 +16,7 @@ export class ScenarioEditorComponent implements OnInit {
   selectedScenario;
   showEditor = false;
   editorLocked = true;
-  
+
 
   constructor(
     private http: HttpClient,
@@ -29,60 +30,62 @@ export class ScenarioEditorComponent implements OnInit {
   }
 
   loadStories() {
-      this.apiService
-        .getStories()
-        .subscribe(resp => {
-          this.stories = resp;
-          console.log('controller: stories loaded', this.stories);
-        });
+    this.apiService
+      .getStories()
+      .subscribe(resp => {
+        this.stories = resp;
+        console.log('controller: stories loaded', this.stories);
+      });
   }
 
   loadStepDefinitions() {
     this.apiService
-        .getStepDefinitions()
-        .subscribe(resp => {
-            this.stepDefinitions = resp;
-            console.log('controller: stepDefinitions loaded', this.stepDefinitions);
-        });
+      .getStepDefinitions()
+      .subscribe(resp => {
+        this.stepDefinitions = resp;
+        console.log('controller: stepDefinitions loaded', this.stepDefinitions);
+      });
   }
 
   updateScenario(storyID) {
     this.apiService
-        .updateScenario(storyID, this.selectedScenario)
-        .subscribe(resp => {
-            console.log('controller:  scenario:', resp);
-        });
+      .updateScenario(storyID, this.selectedScenario)
+      .subscribe(resp => {
+        console.log('controller:  scenario:', resp);
+      });
   }
 
   addScenario(storyID) {
-      this.apiService
-          .addScenario(storyID)
-          .subscribe(resp => {
-            console.log('controller: stepDefinitions loaded', storyID);
-            console.log('storyIDs same?', (storyID === this.selectedStory.story_id));
-            this.stories[this.stories.indexOf(this.selectedStory)].scenarios.push(resp);
+    this.apiService
+      .addScenario(storyID)
+      .subscribe(resp => {
+        console.log('controller: stepDefinitions loaded', storyID);
+        console.log('storyIDs same?', (storyID === this.selectedStory.story_id));
+        this.stories[this.stories.indexOf(this.selectedStory)].scenarios.push(resp);
       });
   }
 
   deleteScenario(event) {
     this.apiService
-        .deleteScenario(this.selectedStory.story_id, this.selectedScenario)
-        .subscribe(resp => {
-            console.log('controller: delete scenario', resp);
-            this.showEditor = false;
+      .deleteScenario(this.selectedStory.story_id, this.selectedScenario)
+      .subscribe(resp => {
+        console.log('controller: delete scenario', resp);
+        this.showEditor = false;
 
-            const indexStory: number = this.stories.indexOf(this.selectedStory);
-            const indexScenario: number = this.stories[indexStory].scenarios.indexOf(this.selectedScenario);
-            if (indexScenario !== -1) {
-                this.stories[indexStory].scenarios.splice(indexScenario, 1);
-            }
-        });
+        const indexStory: number = this.stories.indexOf(this.selectedStory);
+        const indexScenario: number = this.stories[indexStory].scenarios.indexOf(this.selectedScenario);
+        if (indexScenario !== -1) {
+          this.stories[indexStory].scenarios.splice(indexScenario, 1);
+        }
+      });
   }
 
-  addStepToScenario(storyID,step) {
+  addStepToScenario(storyID, step) {
+    var new_id = this.getLastIDinStep(this.selectedScenario.stepDefinitions[0], step.stepType) + 1;
+    alert(new_id);
     console.log('step to add:', step);
-    var new_step = { 
-      id : new Date().valueOf(), //Used as the unique ID of a step (produces something like 1549025639350
+    var new_step = {
+      id: new_id, //Used as the unique ID of a step (produces something like 1549025639350
       label: step.label,
       mid: step.mid,
       pre: step.pre,
@@ -99,70 +102,80 @@ export class ScenarioEditorComponent implements OnInit {
         break;
       case 'then':
         this.selectedScenario.stepDefinitions[0].then.push(new_step);
-         break;
+        break;
       default:
         break;
     }
     console.log('added step', new_step);
   }
 
-  removeStepToScenario(event, stepDefType, index) {
-      console.log('remove step in ' + stepDefType + ' on index ' + index);
-      console.log(stepDefType);
-      switch (stepDefType) {
-          case 'given':
-              this.selectedScenario.stepDefinitions[0].given.splice(index, 1);
-              break;
-          case 'when':
-              this.selectedScenario.stepDefinitions[0].when.splice(index, 1);
-              break;
-          case 'then':
-              this.selectedScenario.stepDefinitions[0].then.splice(index, 1);
-              break;
-      }
+  getLastIDinStep(stepDefs, stepType): number {
+    switch (stepType) {
+      case 'given':
+        return stepDefs.given[stepDefs.given.length - 1].id;
+      case 'when':
+        return stepDefs.when[stepDefs.when.length - 1].id;
+      case 'then':
+        return stepDefs.when[stepDefs.then.length - 1].id;
     }
-  keysList (stepDefs){
-    if (stepDefs != null){
+  }
+  removeStepToScenario(event, stepDefType, index) {
+    console.log('remove step in ' + stepDefType + ' on index ' + index);
+    console.log(stepDefType);
+    switch (stepDefType) {
+      case 'given':
+        this.selectedScenario.stepDefinitions[0].given.splice(index, 1);
+        break;
+      case 'when':
+        this.selectedScenario.stepDefinitions[0].when.splice(index, 1);
+        break;
+      case 'then':
+        this.selectedScenario.stepDefinitions[0].then.splice(index, 1);
+        break;
+    }
+  }
+  keysList(stepDefs) {
+    if (stepDefs != null) {
       return Object.keys(stepDefs);
-    }else {
+    } else {
       return "";
     }
   }
 
-  stepsList(stepDefs,i:number){
-    if (i==0){
+  stepsList(stepDefs, i: number) {
+    if (i == 0) {
       return stepDefs.given;
-    }else if (i==1){
+    } else if (i == 1) {
       return stepDefs.when;
-    }else {
+    } else {
       return stepDefs.then;
     }
   }
 
 
-  addToValues(input:string,stepType,index){
-    switch (stepType){
+  addToValues(input: string, stepType, index) {
+    switch (stepType) {
       case 'given':
-          this.selectedScenario.stepDefinitions[0].given[index].values[0]=input;
+        this.selectedScenario.stepDefinitions[0].given[index].values[0] = input;
         break;
       case 'when':
-          this.selectedScenario.stepDefinitions[0].when[index].values[0]=input;
-      break;
+        this.selectedScenario.stepDefinitions[0].when[index].values[0] = input;
+        break;
       case 'then':
-          this.selectedScenario.stepDefinitions[0].then[index].values[0]=input;
-      break;
+        this.selectedScenario.stepDefinitions[0].then[index].values[0] = input;
+        break;
     }
   }
 
-  getValue (i,values:string[]){
+  getValue(i, values: string[]) {
     return values[i];
   }
 
   renameScenario(event, name) {
-      if (name) {
-          this.selectedScenario.name = name;
-          console.log('controller: changed name of scenario to: ', this.selectedScenario.name);
-      }
+    if (name) {
+      this.selectedScenario.name = name;
+      console.log('controller: changed name of scenario to: ', this.selectedScenario.name);
+    }
   }
 
   lockEditor() {
@@ -182,10 +195,10 @@ export class ScenarioEditorComponent implements OnInit {
   }
 
   selectStory(story) {
-        this.selectedStory = story;
-        this.showEditor = false;
-        this.editorLocked = true;
-        console.log('selected storyID', this.selectedStory);
-    }
+    this.selectedStory = story;
+    this.showEditor = false;
+    this.editorLocked = true;
+    console.log('selected storyID', this.selectedStory);
+  }
 
 }
