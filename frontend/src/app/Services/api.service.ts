79ccc7d4328ff4a618c5a697f6540dba2767994e
Daniel Sorna <daniel.sorna@adesso.de>
2019-05-17 14:58:57 +0200

    moved most dependencies to /backend/package.json

--- frontend/src/app/Services/api.service.ts
+++ frontend/src/app/Services/api.service.ts
@@ -7,7 +7,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 })
 
 export class ApiService {
-  private apiServer: string = 'https://cucumberapp.herokuapp.com/api';
+  private apiServer: string = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
 
