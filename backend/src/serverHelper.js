ba39283c74e500c4cc825583846d2e959e18ba75
Daniel Sorna <daniel.sorna@adesso.de>
2019-08-30 10:24:18 +0200

    trying to fix the chromedriver version issue on heroku

--- backend/src/serverHelper.js
+++ backend/src/serverHelper.js
@@ -17,8 +17,8 @@ const options = {
   metadata: {
     'App Version': '0.3.2',
     'Test Environment': 'STAGING',
-    Browser: 'Chrome  54.0.2840.98',
-    Platform: 'Windows 10',
+    // Browser: 'Chrome  54.0.2840.98',
+    // Platform: 'Windows 10',
     Parallel: 'Scenarios',
     Executed: 'Remote',
   },
