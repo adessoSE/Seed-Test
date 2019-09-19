1f4d4bd066eac92d1cf4608e4943bf0b776d1733
Felix <felix.sommer@adesso.de>
2019-09-19 12:58:20 +0200

    unassigned issues side bar fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -87,6 +87,9 @@ app
           if (issue.assignee !== null) { // skip in case of "unassigned"
             story.assignee = issue.assignee.login;
             story.assignee_avatar_url = issue.assignee.avatar_url;
+          }else{
+            story.assignee = 'unassigned';
+            story.assignee_avatar_url = 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png';
           }
           tmpStories.push(helper.fuseGitWithDb(story, issue.id));
         }
