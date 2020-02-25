ba78503f538edb14845b63953173631c2c77fe67
Felix Sommer <felixissommer@hotmail.com>
2020-02-25 13:52:10 +0100

    fix tests

--- backend/spec/mongodatabase.spec.js
+++ backend/spec/mongodatabase.spec.js
@@ -24,8 +24,10 @@ describe("Mongodatabase", function() {
  describe("showSteptypes", function() {
      it("returns all steptypes", function(done) {
          let stepTypes = [{"_id":null,"id":"","stepType":"when","type":"HoverOverAndSelect","pre":"I hover over the element","mid":"and select the option","values":["",""]},{"_id":null,"id":"","stepType":"when","type":"Textfield","pre":"I insert","mid":"into the field","values":["",""]},{"_id":null,"id":"","stepType":"when","type":"Dropdown","pre":"I select the option","mid":"from the drop-down-menue","values":["",""]},{"_id":null,"id":"","stepType":"then","type":"Website","pre":"So I will be navigated to the website:","mid":"","values":[""]},{"_id":null,"id":"","stepType":"given","type":"Role","pre":"As a","mid":"","values":[""],"selection":["Guest","User"]},{"_id":null,"id":"","stepType":"given","type":"Website","pre":"I am on the website:","mid":"","values":[""]},{"_id":null,"id":"","stepType":"when","type":"Radio","pre":"I select ","mid":"from the selection","values":["",""]},{"_id":null,"id":"","stepType":"when","type":"Checkbox","pre":"I select from the","mid":"multiple selection, the values","values":["",""]},{"_id":null,"id":"","stepType":"example","type":"Add Variable","pre":"","mid":"","values":[""]},{"_id":null,"id":"","stepType":"when","type":"Website","pre":"I go to the website:","mid":"","values":[""]},{"_id":null,"id":"","stepType":"then","type":"Text","pre":"So I can see the text","mid":"in the textbox:","values":["",""]},{"_id":null,"id":"","stepType":"when","type":"Button","pre":"I click the button:","mid":"","values":[""]},{"_id":null,"id":"","stepType":"then","type":"Not This Text","pre":"So I can't see the text:","mid":"","values":[""]},{"_id":null,"id":"","stepType":"given","type":"Undefined Step","pre":"Recommended Title:","mid":"","values":[""]},{"_id":null,"id":"","stepType":"when","type":"Undefined Step","pre":"Recommended Title:","mid":"","values":[""]},{"_id":null,"id":"","stepType":"then","type":"Undefined Step","pre":"Recommended Title:","mid":"","values":[""]}]
+         stepTypes = JSON.stringify(stepTypes);
          mongo.showSteptypes(function(result){
              result.map((s) => s._id = null);
+             result = JSON.stringify(result);
              expect(result).toContain(stepTypes[0]);
              expect(result).toContain(stepTypes[1]);
              expect(result).toContain(stepTypes[2]);
@@ -123,7 +125,7 @@ describe("Mongodatabase", function() {
  //        })
  //      });
  //    it("creates a new scenario", function(done) {
- //        let scenario = {"scenario_id":null,"name":"New Scenario","stepDefinitions":{"given":[],"when":[],"then":[],"example":[]}};
+ //        let scenario = {"scenario_id":null, "comment"=null, name":"New Scenario","stepDefinitions":{"given":[],"when":[],"then":[],"example":[]}};
  //        
  //        mongo.createScenario(storyId, function(result){
  //            scenarioId = result.scenario_id;
