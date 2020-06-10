d4c6fb401cd8625eb8247f30430966d9dec4a28e
Felix Sommer <felixissommer@hotmail.com>
2020-06-10 10:44:59 +0200

    bug fix

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,7 +12,7 @@ export class AppComponent implements OnInit {
   token: string;
   githubName: string;
   title = 'cucumber-frontend';
-  repositories: string[];
+  repositories: RepositoryContainer[];
   githubRepo: string[];
   jiraProject: string[];
   repository: string;
@@ -22,24 +22,7 @@ export class AppComponent implements OnInit {
   error: string;
 
   constructor(public apiService: ApiService, public router: Router) {
-    this.apiService.getProjectsEvent.subscribe((resp: string[]) => {
-      console.log('repositories Event');
-      this.jiraProject = this.filterProjects(resp);
-      if (typeof this.githubRepo !== 'undefined') {
-          this.repositories = this.githubRepo.concat(this.jiraProject);
-      } else {
-          this.repositories = this.jiraProject;
-      }
-    });
-    this.apiService.getRepositoriesEvent.subscribe((resp: string[]) => {
-      console.log('repositories Event');
-      this.githubRepo = resp;
-      if (typeof this.jiraProject !== 'undefined') {
-        this.repositories = this.githubRepo.concat(this.jiraProject);
-      } else {
-        this.repositories = this.githubRepo;
-      }
-    });
+
   }
 
   ngOnInit() {
@@ -68,10 +51,9 @@ export class AppComponent implements OnInit {
   }
 
   getRepositories() {
-    let tmp_repositories = [];
     if (this.apiService.isLoggedIn() && (typeof this.repositories === 'undefined' || this.repositories.length > 0)) {
       this.apiService.getRepositories().subscribe((resp) => {
-        tmp_repositories = resp;
+        this.repositories = resp;
       }, (err) => {
         this.error = err.error;
       });
