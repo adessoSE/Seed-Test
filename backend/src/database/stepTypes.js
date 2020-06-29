4fa4d139b077f91ee8264a0bb419c5c9b8c3e032
Felix Sommer <felixissommer@hotmail.com>
2020-06-29 10:28:00 +0200

    update stepTypes

--- backend/src/database/stepTypes.js
+++ backend/src/database/stepTypes.js
@@ -1,143 +1,193 @@
 function stepDefs() {
-  return [
-    // #################### GIVEN ########################################
-    {
-      id: '',
-      stepType: 'given',
-      type: 'Role',
-      pre: 'As a',
-      mid: '',
-      values: [''],
-      selection: ['Guest', 'User'],
-    },
-    {
-      id: '',
-      stepType: 'given',
-      type: 'Website',
-      pre: 'I am on the website:',
-      mid: '',
-      values: [''],
-    },
-    {
-      id: '',
-      stepType: 'example',
-      type: 'Add Variable',
-      pre: '',
-      mid: '',
-      values: [''],
-    },
-    // ################### WHEN ##########################################
-    {
-      id: '',
-      stepType: 'when',
-      type: 'Website',
-      pre: 'I go to the website:',
-      mid: '',
-      values: [''],
-    },
-    {
-      id: '',
-      stepType: 'when',
-      type: 'Button',
-      pre: 'I click the button:',
-      mid: '',
-      values: [''],
-    },
-    {
-      id: '',
-      stepType: 'when',
-      type: 'Field',
-      pre: 'I insert',
-      mid: 'into the field',
-      values: ['', ''],
-    },
-    {
-      id: '',
-      stepType: 'when',
-      type: 'Radio',
-      pre: 'I select ',
-      mid: 'from the selection',
-      values: ['', ''],
-    },
-    {
-      id: '',
-      stepType: 'when',
-      type: 'Dropdown',
-      pre: 'I select the option',
-      mid: 'from the drop-down-menue',
-      values: ['', ''],
-    },
-    {
-      id: '',
-      stepType: 'when',
-      type: 'HoverOverAndSelect',
-      pre: 'I hover over the element',
-      mid: 'and select the option',
-      values: ['', ''],
-    },
-    {
-      id: '',
-      stepType: 'when',
-      type: 'ListSelection',
-      pre: 'I select from the',
-      mid: 'multiple selection, the values',
-      values: ['', ''],
-    },
-    {
-      id: '',
-      stepType: 'when',
-      type: 'Wait',
-      pre: 'The application should wait for',
-      mid: 'milliseconds',
-      values: [''],
-    },
-    {
-      id: '',
-      stepType: 'when',
-      type: 'Checkbox',
-      pre: 'I check the box',
-      mid: '',
-      values: [''],
-    },
-    // ################### THEN ##########################################
-    {
-      id: '',
-      stepType: 'then',
-      type: 'Website',
-      pre: 'So I will be navigated to the website:',
-      mid: '',
-      values: [''],
-    },
-    {
-      id: '',
-      stepType: 'then',
-      type: 'Textbox',
-      pre: 'So I can see the text',
-      mid: 'in the textbox: ',
-      values: ['', ''],
-    },
-    {
-      id: '',
-      stepType: 'then',
-      type: 'This Text',
-      pre: 'So I can see the text:',
-      values: [''],
-    },
-    {
-      id: '',
-      stepType: 'then',
-      type: 'Not this Text',
-      pre: 'So I can\'t see the text:',
-      values: [''],
-    },
-    {
-      id: '',
-      stepType: 'then',
-      type: 'Empty Textbox',
-      pre: 'So I can see the text in the textbox:',
-      values: [''],
-    },
-  ];
+  return [{
+    "id": 0,
+    "stepType": "example",
+    "type": "Add Variable",
+    "pre": "",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+  
+    "id": 0,
+    "stepType": "when",
+    "type": "HoverOverAndSelect",
+    "pre": "I hover over the element",
+    "mid": "and select the option ",
+    "values": [
+      "",
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "given",
+    "type": "Website",
+    "pre": "I am on the website:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "when",
+    "type": "Website",
+    "pre": "I go to the website:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "when",
+    "type": "Button",
+    "pre": "I click the button:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "when",
+    "type": "Textfield",
+    "pre": "I insert",
+    "mid": "into the field ",
+    "values": [
+      "",
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "then",
+    "type": "Website",
+    "pre": "So I will be navigated to the website:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "when",
+    "type": "Radio",
+    "pre": "I select",
+    "mid": "from the selection ",
+    "values": [
+      "",
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "when",
+    "type": "ListSelection",
+    "pre": "I select from the",
+    "mid": "multiple selection, the values ",
+    "values": [
+      "",
+      "",
+      "",
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "given",
+    "type": "Role",
+    "pre": "As a",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "then",
+    "type": "Textbox",
+    "pre": "So I can see the text",
+    "mid": "in the textbox: ",
+    "values": [
+      "",
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "when",
+    "type": "Dropdown",
+    "pre": "I select the option",
+    "mid": "from the drop-down-menue ",
+    "values": [
+      "",
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "then",
+    "type": "Not this Text",
+    "pre": "So I can't see the text:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "given",
+    "type": "New Step",
+    "pre": "Recommended Title:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "when",
+    "type": "New Step",
+    "pre": "Recommended Title:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "then",
+    "type": "New Step",
+    "pre": "Recommended Title:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": 0,
+    "stepType": "then",
+    "type": "This Text",
+    "pre": "So I can see the text:",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  },{
+    "id": "0",
+    "stepType": "when",
+    "type": "Wait",
+    "pre": "The site should wait for",
+    "mid": "milliseconds",
+    "values": [
+      ""
+    ]
+  },{
+    "id": "0",
+    "stepType": "then",
+    "type": "Empty Textbox",
+    "pre": "So I can see the text in the textbox:",
+    "values": [
+      ""
+    ]
+  },{
+    "id": "",
+    "stepType": "when",
+    "type": "Checkbox",
+    "pre": "I check the box",
+    "mid": "",
+    "values": [
+      ""
+    ]
+  }];
 }
 
 module.exports = stepDefs;
