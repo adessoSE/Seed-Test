6a8040f08237bfc40897335f7124f4879be7ed95
Daniel Sorna <daniel.sorna@adesso.de>
2020-12-02 15:25:34 +0100

    refactroring and some vulnerabilities fixed fixed depenency bug

--- backend/src/passport-config.js
+++ backend/src/passport-config.js
@@ -1,47 +1,42 @@
 // https://github.com/WebDevSimplified/Nodejs-Passport-Login/blob/master/passport-config.js
 const LocalStrategy = require('passport-local').Strategy;
-var GitHubStrategy = require('passport-github2').Strategy;
 const bcrypt = require('bcrypt');
 
 function initialize(passport, getUserByEmail, getUserById, getUserByGithub) {
-    const authenticateUser = async(email, password, done) => {
+	const authenticateUser = async (email, password, done) => {
+		// not working yet , needs callback, or database change to Promises
+		const user = await getUserByEmail(email);
 
-        // not working yet , needs callback, or database change to Promises 
-        const user = await getUserByEmail(email);
+		if (!user) return done(null, false, { message: 'No user with this email' });
 
-        if(!user){
-            return done(null, false, {message: 'No user with this email'});
-        }
 
-        try {
-            if(await bcrypt.compare(password, user.password)){
-                return done(null, user);
-            }else {
-                return done(null, false, {message: 'Password incorrect'});
-            }
-        } catch(e){
-            console.log('error: ' + e)
-            return done(e);
-        }
-    }
+		try {
+			if (await bcrypt.compare(password, user.password)) return done(null, user);
 
-    const authenticateUserGithub = async(login, id, done) => {
-        const user = await getUserByGithub(login, id);
-        if(!user){
-            return done(null, false, {message: 'No user with this Github Account'});
-        }
-        return done(null, user)
-    }
-    
+			return done(null, false, { message: 'Password incorrect' });
+		} catch (e) {
+			console.log(`error: ${e}`);
+			return done(e);
+		}
+	};
 
-    passport.use('normal-local', new LocalStrategy({ usernameField: 'email' }, authenticateUser));
-    passport.use('github-local', new LocalStrategy({ usernameField: 'login', passwordField: 'id',}, authenticateUserGithub));
-    
-    passport.serializeUser((user, done) => done(null, user._id));
-    passport.deserializeUser(async (id, done) => {
-        let user = await getUserById(id);
-        return done(null, user);
-    })
+	const authenticateUserGithub = async (login, id, done) => {
+		const user = await getUserByGithub(login, id);
+		if (!user) return done(null, false, { message: 'No user with this Github Account' });
+
+		return done(null, user);
+	};
+
+
+	passport.use('normal-local', new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+	passport.use('github-local', new LocalStrategy({ usernameField: 'login', passwordField: process.env.PASSPORT_GITHUB_LOCAL_PW_FIELD }, authenticateUserGithub));
+
+	// eslint-disable-next-line no-underscore-dangle
+	passport.serializeUser((user, done) => done(null, user._id));
+	passport.deserializeUser(async (id, done) => {
+		const user = await getUserById(id);
+		return done(null, user);
+	});
 }
 
 module.exports = initialize;
