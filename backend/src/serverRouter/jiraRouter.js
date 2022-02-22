b20037cd31f3fced6e2ed1df6aa6a7eb30e18393
Daniel Sorna <daniel.sorna@adesso.de>
2022-02-22 17:57:56 +0100

    fixed fetch import and adjusted height of story-bar and -editor

--- backend/src/serverRouter/jiraRouter.js
+++ backend/src/serverRouter/jiraRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const helper = require('../serverHelper');
 
 const router = express.Router();
