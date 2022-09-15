7fc3e3611b92bb82cc3fbb417ae59cd9f0c963f1
Köhler, Jonathan <Jonathan.Koehler@adesso.de>
2022-09-15 14:26:36 +0200

    updated function call

--- backend/src/serverHelper.js
+++ backend/src/serverHelper.js
@@ -587,10 +587,11 @@ async function runReport(req, res, stories, mode, parameters) {
 					postCommentGitHub(story.issue_number, comment, githubName, githubRepo, req.user.github.githubToken)
 					if (mode === 'feature') updateLabel('testStatus', githubName, githubRepo, req.user.github.githubToken, story.issue_number);
 				}
-				// if (story.storySource === 'jira' && req.user && req.user.jira){
-				//	const password = decryptPassword(req.user.jira.Password);
-				// 	postCommentJira(story.issue_number, comment, req.user.jira.Host, req.user.jira.AccountName, password)
-				// }
+				if (story.storySource === 'jira' && req.user && req.user.jira){
+					let { Host, AccountName, Password, Password_Nonce, Password_Tag} = req.user.jira;
+					Password = decryptPassword(Password, Password_Nonce, Password_Tag);
+					postCommentJira(story.issue_number, comment, Host, AccountName, Password)
+				}
 			}
 		});
 		
