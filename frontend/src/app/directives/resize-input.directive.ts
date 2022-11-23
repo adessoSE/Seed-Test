fb916f6d95237fadc30bb41a798d60a866367833
Alice-cla <doll.alice00@gmail.com>
2022-11-23 22:46:40 +0100

    Resize input fix

--- frontend/src/app/directives/resize-input.directive.ts
+++ frontend/src/app/directives/resize-input.directive.ts
@@ -19,11 +19,11 @@ export class ResizeInputDirective {
   @HostBinding() class?;
 
   @HostListener('change', ['$event']) onChange() {
-    this.resize('action');
+    this.resize();
   }
 
   @HostListener('input', ['$event']) onInput() {
-    this.resize('action');
+    this.resize();
   }
 
  
@@ -48,39 +48,37 @@ export class ResizeInputDirective {
    * Resize input filed in backgroung or scenario on input string length
    * @param mode_type 
    */
-  private resize(mode_type: string) {
+  private resize(mode_type?: string) {
     //Set variables
     let parentWidth = this.setParentWidth();
-    
     let string_coef = 4;
     let string_length = this.el.nativeElement.value.length+string_coef;
     let input_width = this.el.nativeElement.offsetWidth;
     let coef = 10;
-    
+    let gap = (this.maxWidth - parentWidth);
+
     //Check if maxWidth exceeded 
     if((parentWidth - input_width + string_length*coef) < this.maxWidth) {
-      if (string_length == 0 ){
+      if (string_length <= 10 ){
         this.el.nativeElement.setAttribute('size', this.minWidth);
       } else {
-        this.el.nativeElement.setAttribute('size', string_length);
+        this.el.nativeElement.setAttribute('size', string_length-string_coef);
       }
-    }  
+    } 
     else {
-      let gap = (this.maxWidth - parentWidth);
-      if (gap > 0){
-
-        if (mode_type == 'action') {
-          this.el.nativeElement.setAttribute('size', this.el.nativeElement.getAttribute('size'));
-        } 
+      if (gap >= 0){
         if (mode_type == 'load') {
           let width = (input_width + gap)/coef;
           this.el.nativeElement.setAttribute('size', width);
         } 
-      }
+      } 
       else {
-        this.el.nativeElement.setAttribute('size', this.minWidth);
+        if (mode_type == 'load') {
+          let width = (input_width - gap)/coef;
+          this.el.nativeElement.setAttribute('size', width);
+        } 
       }
-    }
+    } 
   }
 
   /**
