48511056f58a0004ef3e1d6127cab667a0e47d2f
i3rotlher <maxe.kw@googlemail.com>
2022-12-20 09:46:29 +0100

    standalone MongoDB

--- backend/src/database/installDatabase.js
+++ backend/src/database/installDatabase.js
@@ -1,15 +1,31 @@
 const { MongoClient } = require('mongodb');
+const { resolve } = require('path');
+const { exit } = require('process');
 const stepTypes = require('./stepTypes');
 require('dotenv').config();
 
 const uri = process.env.DATABASE_URI;
 
+async function checkConnection() {
+	var fails = 1;
+	await MongoClient.connect(uri, { poolSize: 20, useNewUrlParser: true, useUnifiedTopology: true }, async (err, dbo) => {
+		if (err) {
+			console.log("Connection failed ! retrying ... " + fails);
+			fails++;
+			checkConnection()
+		}
+	});
+}
+
 async function installDatabase() {
 	console.log (`Setting Up DB in: ${uri}`);
+	await checkConnection()
+	console.log("Starting: steps");
 	await makeCollection('stepTypes');
+	console.log("Starting: stories");
 	await makeCollection('Stories');
+	console.log("Starting: user");
 	await makeCollection('User');
-	await insertMore('stepTypes', stepTypes());
 }
 
 // create Collection
@@ -23,10 +39,23 @@ async function makeCollection(name) {
 	// sleep 3000 ms
 	const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 	await delay(3000);
-	await connection.createCollection(name, (error) => {
-		if (error) throw error;
+	await new Promise((resolve) => { connection.createCollection(name, (error) => {
+		if (error) {
+			if (error.message.includes("already exists")) {
+				console.log("Collection: " + name + " already exists and will be skipped.");
+				resolve();
+				return;
+			} else {
+				throw error;
+			}
+		};
 		console.log(`Collection ${name} created!`);
-	});
+		if (name === "stepTypes") {
+			console.log("Adding StepTypes: ");
+			insertMore('stepTypes', stepTypes());
+		}
+		resolve();
+	})});
 }
 
 // insert Many documents ("collectionname", [{documents},{...}] )
@@ -37,9 +66,8 @@ function insertMore(name, content) {
 		dbo.collection(name).insertMany(content, (error, res) => {
 			if (error) throw error;
 			console.log(`Number of documents inserted: ${res.insertedCount}`);
-			db.close();
 		});
 	});
 }
 
-installDatabase();
+installDatabase().then(() => {exit()})
