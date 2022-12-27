bfbf474e4b3d0ed9f71340e1d6877d9cea9b37f8
Köhler, Jonathan <Jonathan.Koehler@adesso.de>
2022-12-27 17:15:03 +0100

    place order, result, status; listen output; untested

--- backend/src/serverRouter/kiRouter.js
+++ backend/src/serverRouter/kiRouter.js
@@ -1,15 +1,46 @@
-var fs = require('fs');
+var fs = require('fs/promises');
 var path = require('path');
 
 const KI_Exchange = process.env.KI_Exchange
 
+fs.watch(path.join(KI_Exchange, 'output'),[], async (eventType, filename)=>{
+    file = (await getFile(path.join(KI_Exchange, 'output', filename))).toJSON();
+    consumeKiOutput(file)
+})//watch the output directory
+
 
 function placeKiOrder(story) {
-    fs.writeFile(path.join(KI_Exchange, 'input',story._id+'.json'),JSON.stringify(story),
+    placeFile('input', story._id+'.json', JSON.stringify(story));
+    console.log('place ki order');
+}
+
+function placeResult(result, storyId){
+    placeFile('result', storyId+'.json', result);
+    console.log('place Result');
+}
+
+function placeStatus(status){
+    placeFile('status', 'Seed'+Date.now()+'.json', status);
+}
+
+function getFile(dir){
+    return fs.readFile(path.normalize(dir))
+}
+
+function consumeKiOutput(story){
+
+}
+
+function placeFile(dir, filename, file){
+    fs.writeFile(path.join(KI_Exchange, dir, filename),file,
     ()=>{
         console.log('wrote file');
     });
-    console.log('place ki order');
+}
+
+function consumeFile(){
+    
+
 }
 
 module.exports = {placeKiOrder}
