8f4aafaf85d14867dcdbdb10a1c2daec42cafb72
Köhler, Jonathan <Jonathan.Koehler@adesso.de>
2023-01-02 10:15:38 +0100

    better listen

--- backend/src/serverRouter/kiRouter.js
+++ backend/src/serverRouter/kiRouter.js
@@ -1,17 +1,37 @@
-var fs = require('fs/promises');
+var pfs = require('fs/promises');
 var path = require('path');
+var db = require('../database/DbServices')
 
 const KI_Exchange = process.env.KI_Exchange
 
-fs.watch(path.join(KI_Exchange, 'output'),[], async (eventType, filename)=>{
-    file = (await getFile(path.join(KI_Exchange, 'output', filename))).toJSON();
-    consumeKiOutput(file)
-})//watch the output directory
+
+
+
+const ac = new AbortController();
+const { signal } = ac;
+// setTimeout(() => ac.abort(), 10000);
+
+(async () => {
+  try {
+    const watcher = pfs.watch(path.join(KI_Exchange, 'output'), { signal });
+    for await (const fsEvent of watcher)
+      console.log(fsEvent);
+      const file = await pfs.readFile(path.join(KI_Exchange, 'output', fsEvent.filename)).then(()=>console.log('read file'));
+      console.log(file);
+      consumeKiOutput(file)
+  } catch (err) {
+    console.error(err);
+    if (err.name === 'AbortError')
+      return;
+    throw err;
+  }
+})();
+console.log('watch ki-exchange output');
 
 
 function placeKiOrder(story) {
     placeFile('input', story._id+'.json', JSON.stringify(story));
-    console.log('place ki order');
+    console.log('place kI Order');
 }
 
 function placeResult(result, storyId){
@@ -23,24 +43,20 @@ function placeStatus(status){
     placeFile('status', 'Seed'+Date.now()+'.json', status);
 }
 
-function getFile(dir){
-    return fs.readFile(path.normalize(dir))
+async function getFile(dir){
+    return await pfs.readFile(dir)
 }
 
 function consumeKiOutput(story){
-
+    console.log('consume KI Output: ' + JSON.stringify(story));
+    //db.updateStory(story);
 }
 
 function placeFile(dir, filename, file){
-    fs.writeFile(path.join(KI_Exchange, dir, filename),file,
+    pfs.writeFile(path.join(KI_Exchange, dir, filename),file,
     ()=>{
         console.log('wrote file');
     });
 }
 
-function consumeFile(){
-    
-
-}
-
-module.exports = {placeKiOrder}
+module.exports = {placeKiOrder,placeResult}
