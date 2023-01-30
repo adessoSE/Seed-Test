43eb3ffe486152bf3e50f36fcf205e2342931fc9
i3rotlher <maxe.kw@googlemail.com>
2023-01-30 18:04:31 +0100

    use default port for cors and docker

--- runDocker.bat
+++ runDocker.bat
@@ -6,6 +6,7 @@ for /f "tokens=*" %%s in (./backend/.env) do (
   set %%s
 )
 
+set "PORT=%PORT:8080%"
 echo BACKENDPORT=%PORT%>.env
 
 @echo off
@@ -13,9 +14,10 @@ for /f "tokens=*" %%s in (./frontend/.env) do (
   set %%s
 )
 
+set "PORT=%PORT:4200%"
 echo FRONTENDPORT=%PORT%>>.env
 
 docker compose down
 docker rmi seed-test-backend
 docker rmi seed-test-frontend
-docker-compose up --build
+docker compose up --build
