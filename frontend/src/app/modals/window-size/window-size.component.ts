288c7db3958fb0966f96a38d3b2bd21055de1ccc
Marc <marcbessah@gmail.com>
2023-11-30 17:41:50 +0100

    fix navigate between input fields of window size with tab key

--- frontend/src/app/modals/window-size/window-size.component.ts
+++ frontend/src/app/modals/window-size/window-size.component.ts
@@ -6,38 +6,22 @@ import { MatMenuTrigger } from '@angular/material/menu';
   templateUrl: './window-size.component.html',
   styleUrls: ['./window-size.component.css']
 })
-export class WindowSizeComponent implements OnInit {
-
-  shouldStopPropagation: boolean = true;
+export class WindowSizeComponent {
 
   @ViewChild('appMenu') menuTrigger: MatMenuTrigger;
   @Input() width: number;
   @Input() height: number;
   @Output() sizeChange = new EventEmitter<{ width: number, height: number }>
 
-  ngOnInit() {
-    console.log(this.width, this.height)
-  }
 
   setDefaultWindowSize(): void {
     this.width = 1920;
     this.height = 1080;
   }
-
-  handleFormClick(event: Event) {
-    if (this.shouldStopPropagation) {
-        event.stopPropagation();
-    }
-  }
   
   saveWindowSize(event: Event): void {
     console.log(this.width, this.height)
     this.sizeChange.emit({ width: this.width, height: this.height })
-    this.shouldStopPropagation = false;
-  }
-
-  closeMenu():void {
-    this.shouldStopPropagation = false;
-  }
+   }
 
 }
