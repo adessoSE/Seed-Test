fdebf5970030cc01692ecb3a21ae5f145e2a78b9
i3rotlher <maxe.kw@googlemail.com>
2024-01-08 15:03:43 +0100

    added api acll to hover

--- frontend/src/app/Services/highlight-input.service.ts
+++ frontend/src/app/Services/highlight-input.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
+import { Injectable } from "@angular/core";
+import { ToastrService } from "ngx-toastr";
+import { ApiService } from "./api.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class HighlightInputService {
-
-  constructor(public toastr: ToastrService) { }
+  constructor(public toastr: ToastrService, public apiService: ApiService) {}
   targetOffset: number = 0;
 
   /**
@@ -19,58 +19,84 @@ export class HighlightInputService {
    * @param initialCall if call is from ngAfterView
    * @param isDark theming Service
    * @param regexInStory if first regex in Story
-   * @param valueIndex index of input field 
+   * @param valueIndex index of input field
    * @param stepPre pre text of step
    * @returns if a regex was detected
    */
-  highlightRegex(element, initialCall?:boolean, isDark?:boolean, regexInStory?:boolean, valueIndex?: number, stepPre?: string) {
-    const regexPattern =/(\{Regex:)(.*?)(\})(?=\s|$)/g;// Regex pattern to recognize and highlight regex expressions -> start with {Regex: and end with }
+  highlightRegex(
+    element,
+    initialCall?: boolean,
+    isDark?: boolean,
+    regexInStory?: boolean,
+    valueIndex?: number,
+    stepPre?: string
+  ) {
+    const regexPattern = /(\{Regex:)(.*?)(\})(?=\s|$)/g; // Regex pattern to recognize and highlight regex expressions -> start with {Regex: and end with }
 
-    const textField = element
+    const textField = element;
     const textContent = textField.textContent;
     //Get current cursor position
-    const offset = this.getCaretCharacterOffsetWithin(textField)
-    const regexSteps = ['So I can see the text', 'So I can see the text:', 'So I can\'t see the text:', 'So I can\'t see text in the textbox:']
+    const offset = this.getCaretCharacterOffsetWithin(textField);
+    const regexSteps = [
+      "So I can see the text",
+      "So I can see the text:",
+      "So I can't see the text:",
+      "So I can't see text in the textbox:",
+    ];
     var regexDetected = false;
-
+    var identifier = `id${Date.now()}`;
     let highlightedText;
-    if(!valueIndex || (0==valueIndex && regexSteps.includes(stepPre))){
-      if(isDark){
-        highlightedText = textContent.replace(regexPattern, (match, match1, match2, match3) => {
+    // TODO: Hardcoded Styles
+    if (!valueIndex || (0 == valueIndex && regexSteps.includes(stepPre))) {
+      highlightedText = textContent.replace(
+        regexPattern,
+        (match, match1, match2, match3) => {
           regexDetected = true;
-          return `<span uk-tooltip="title:Regular Expression detected!;pos:right">`+
-          `<span style="color: var(--light-grey); font-weight: bold">${match1}</span>`+
-          `<span style="color: var(--light-blue); font-weight: bold">${match2}</span>`+
-          `<span style="color: var(--light-grey); font-weight: bold">${match3}</span></span>`;
-        });
-      } else{
-        highlightedText = textContent.replace(regexPattern, (match, match1, match2, match3) => {
-          regexDetected = true;
-          return `<span uk-tooltip="title:Regular Expression detected!;pos:right">`+
-          `<span style="color: var(--brown-grey); font-weight: bold">${match1}</span>`+
-          `<span style="color: var(--ocean-blue); font-weight: bold">${match2}</span>`+
-          `<span style="color: var(--brown-grey); font-weight: bold">${match3}</span></span>`;
-        });
-      }
+          this.apiService
+            .resolveSpecialCommand(match2)
+            .subscribe((resolvedCommand) => {
+              console.log("RECIEVED: " + resolvedCommand);
+              document
+                .querySelector(`#${identifier}`)
+                .setAttribute("uk-tooltip", resolvedCommand);
+            });
+          return (
+            `<span uk-tooltip="Resolving Command ..." id="${identifier}">` +
+            `<span style="color: ${
+              isDark ? "var(--light - grey)" : "var(--brown-grey)"
+            }; font-weight: bold">${match1}</span>` +
+            `<span style="color: ${
+              isDark ? "var(--light-blue)" : "var(--ocean-blue)"
+            }; font-weight: bold">${match2}</span>` +
+            `<span style="color: ${
+              isDark ? "var(--light-grey)" : "var(--brown-grey)"
+            }; font-weight: bold">${match3}</span></span>`
+          );
+        }
+      );
     }
     textField.innerHTML = highlightedText ? highlightedText : textContent;
 
     // Toastr logic
-    if(initialCall && regexDetected) {
-      regexInStory = true
+    if (initialCall && regexDetected) {
+      regexInStory = true;
     }
-    if(regexDetected && !regexInStory){
-      this.toastr.info('View our Documentation for more Info','Regular Expression detected!');
+    if (regexDetected && !regexInStory) {
+      this.toastr.info(
+        "View our Documentation for more Info",
+        "Regular Expression detected!"
+      );
     }
 
     // Set cursor to correct position
-    if(!initialCall){
-      if (regexDetected) { //maybe not needed
+    if (!initialCall) {
+      if (regexDetected) {
+        //maybe not needed
         const selection = window.getSelection();
-        selection.removeAllRanges()
+        selection.removeAllRanges();
 
         // Call the function to find the correct node and offset
-        this.targetOffset = offset
+        this.targetOffset = offset;
         const result = this.findNodeAndOffset(textField);
 
         if (result !== null) {
@@ -81,7 +107,12 @@ export class HighlightInputService {
               selection.setBaseAndExtent(node, offsetIndex, node, offsetIndex);
             } else if (node.nodeType === 1 && node.childNodes.length > 0) {
               // Element node with child nodes (e.g., <span>)
-              selection.setBaseAndExtent(node.childNodes[0], offsetIndex, node.childNodes[0], offsetIndex);
+              selection.setBaseAndExtent(
+                node.childNodes[0],
+                offsetIndex,
+                node.childNodes[0],
+                offsetIndex
+              );
             }
           });
         }
@@ -89,18 +120,23 @@ export class HighlightInputService {
         requestAnimationFrame(() => {
           const selection = window.getSelection();
           selection.removeAllRanges();
-          selection.setBaseAndExtent(textField.firstChild, offset, textField.firstChild, offset)
-        })
+          selection.setBaseAndExtent(
+            textField.firstChild,
+            offset,
+            textField.firstChild,
+            offset
+          );
+        });
       }
     }
     return regexDetected;
   }
 
-/**
- * Helper for Regex Highlighter, find right node and index for current cursor position
- * @param element HTMLElement
- * @returns node: node with cursor, number: offest of cursor in node
- */
+  /**
+   * Helper for Regex Highlighter, find right node and index for current cursor position
+   * @param element HTMLElement
+   * @returns node: node with cursor, number: offest of cursor in node
+   */
   findNodeAndOffset(element: Node): [Node, number] | null {
     if (element.nodeType === 3) {
       // Text node
@@ -110,7 +146,7 @@ export class HighlightInputService {
       } else {
         this.targetOffset -= textLength;
       }
-    } else if (element.nodeType === 1){
+    } else if (element.nodeType === 1) {
       // Element node
       for (let i = 0; i < element.childNodes.length; i++) {
         const child = element.childNodes[i];
@@ -134,20 +170,20 @@ export class HighlightInputService {
     var win = doc.defaultView || doc.parentWindow;
     var sel;
     if (typeof win.getSelection != "undefined") {
-        sel = win.getSelection();
-        if (sel.rangeCount > 0) {
-            var range = win.getSelection().getRangeAt(0);
-            var preCaretRange = range.cloneRange();
-            preCaretRange.selectNodeContents(element);
-            preCaretRange.setEnd(range.endContainer, range.endOffset);
-            caretOffset = preCaretRange.toString().length;
-        }
-    } else if ( (sel = doc.selection) && sel.type != "Control") {
-        var textRange = sel.createRange();
-        var preCaretTextRange = doc.body.createTextRange();
-        preCaretTextRange.moveToElementText(element);
-        preCaretTextRange.setEndPoint("EndToEnd", textRange);
-        caretOffset = preCaretTextRange.text.length;
+      sel = win.getSelection();
+      if (sel.rangeCount > 0) {
+        var range = win.getSelection().getRangeAt(0);
+        var preCaretRange = range.cloneRange();
+        preCaretRange.selectNodeContents(element);
+        preCaretRange.setEnd(range.endContainer, range.endOffset);
+        caretOffset = preCaretRange.toString().length;
+      }
+    } else if ((sel = doc.selection) && sel.type != "Control") {
+      var textRange = sel.createRange();
+      var preCaretTextRange = doc.body.createTextRange();
+      preCaretTextRange.moveToElementText(element);
+      preCaretTextRange.setEndPoint("EndToEnd", textRange);
+      caretOffset = preCaretTextRange.text.length;
     }
     return caretOffset;
   }
