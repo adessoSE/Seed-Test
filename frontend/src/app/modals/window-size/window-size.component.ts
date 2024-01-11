c52617e008f01299f5be4e3fb0cefbb585e53c09
Marc <marcbessah@gmail.com>
2024-01-11 22:40:41 +0100

    Implemented predefined resolution selection in window size component

--- frontend/src/app/modals/window-size/window-size.component.ts
+++ frontend/src/app/modals/window-size/window-size.component.ts
@@ -9,20 +9,44 @@ import { MatMenuTrigger } from '@angular/material/menu';
 export class WindowSizeComponent {
 
   @ViewChild('appMenu') menuTrigger: MatMenuTrigger;
-  @Input() width: number;
-  @Input() height: number;
-  @Input() emulator: boolean;
-  @Output() sizeChange = new EventEmitter<{ width: number, height: number }>
+  @Input() width: number;  
+  @Input() height: number;  
+  @Input() emulator: boolean;  
+  @Output() sizeChange = new EventEmitter<{ width: number, height: number }>();
 
+  selectedResolution: string;
+  predefinedResolutions = ["3840x2160", "2560x1440", "1920x1080", "1600x900", "1280x720"];
 
+  ngOnInit() {
+    // Initialize the selected resolution based on the current width and height
+    this.updateSelectedResolution();
+  }
+
+  // Updates the selected resolution; sets to 'custom' if the current resolution is not in the predefined list
+  updateSelectedResolution() {
+    const currentResolution = `${this.width}x${this.height}`;
+    if (this.predefinedResolutions.includes(currentResolution)) {
+      this.selectedResolution = currentResolution;
+    } else {
+      this.selectedResolution = 'custom';
+    }
+  }
+
+  // Sets the default window size and updates the selected resolution accordingly
   setDefaultWindowSize(): void {
     this.width = 1920;
     this.height = 1080;
+    this.selectedResolution = `${this.width}x${this.height}`;
   }
-  
+
+  // Saves the current window size and emits a size change event
   saveWindowSize(event: Event): void {
-    console.log(this.width, this.height)
-    this.sizeChange.emit({ width: this.width, height: this.height })
-   }
+    if (this.selectedResolution !== 'custom') {
+      const [resWidth, resHeight] = this.selectedResolution.split('x').map(Number);
+      this.width = resWidth;
+      this.height = resHeight;
+    }
+    this.sizeChange.emit({ width: this.width, height: this.height });
+  }
 
 }
