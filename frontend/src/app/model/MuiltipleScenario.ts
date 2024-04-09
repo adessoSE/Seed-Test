fa86ba1cba416f01441602e352e1c90c9f922ac6
Nataliia Karmazina <nataliia.karmazina@adesso.de>
2024-04-09 12:10:19 +0200

    [Frontend] new data model for multiple scenarios

---
+++ frontend/src/app/model/MuiltipleScenario.ts
@@ -0,0 +1,25 @@
+/**
+ * Interface of a Multiple Scenario
+ */
+export interface MultipleScenario{
+
+    /**
+    * example key/name
+    */
+    key: string;
+
+    /**
+     * example value
+     */
+    value: string[];
+
+    /**
+     * checked 
+     */
+    checked?: boolean;
+
+    /**
+     * deactivated currently not used
+     */
+    deactivated?: boolean;
+}
