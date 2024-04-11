1b3758de046233f33d13faa8e12addbde4523795
Silas Meilbeck <silas.meilbeck@adesso.de>
2024-04-11 19:58:01 +0200

    Updated to Angular16; entryComponents: [InfoWarningToast] from app.module.ts removed! AUTH Guard needs to be ADJUSTED!

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { LoginService } from '../Services/login.service';
 
 /**
  * Guard to check if the user is logged in
  */
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
     /**
      * Constructor
