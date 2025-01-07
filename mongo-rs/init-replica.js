4b7d689f0ff740aaa9063b9ce9933539bcf31683
Jonathan Köhler <jonathan.koehler@adesso.de>
2025-01-07 17:05:37 +0100

    remove script. fails at rs comand.

--- mongo-rs/init-replica.js
+++
@@ -1,5 +0,0 @@
-console.log("Initiate Replica-Set")
-delay(20000)
-rs.initiate({_id: "rs0", version: 1, members: [{ _id: 0, host: "mongo1:27017" }, { _id: 1, host: "mongo2:27018" }, { _id: 2, host: "mongo3:27019" }]})
-console.log("initiated? ")
-console.log(rs.status())
